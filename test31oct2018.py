9c858f3defc8b90597798b2147cb65144e69a2ea
MunYeen <munyeen.chong@pslgroup.com>
2018-10-31 10:24:43 +0800

    Date Functions

---
+++ test31oct2018.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import datetime
+
+currDate = datetime.date.today()
+print (currDate)
+print (currDate.year)
+print (currDate.month)
+print (currDate.day)
+
+#print (currDate.strftime('%d %b, %y'))
+
+print (currDate.strftime('%d %b, %Y'))
+
+print (currDate.strftime('%A %d %B, %Y'))
+
+
+
