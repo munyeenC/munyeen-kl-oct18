79b4c537cae10cae74d21285efa50fb9e3f863c1
MunYeen <munyeen.chong@pslgroup.com>
2018-10-31 10:57:20 +0800

    Date Functions

--- birthday.py
+++ birthday.py
@@ -7,10 +7,14 @@ Created on Wed Oct 31 10:42:05 2018
 
 import datetime
 
-birthday = input ("What is your birthday? ")
+#birthday = input ("What is your birthday? (dd/mm/YYYY)")
 
-birthdate = datetime.datetime.strptime(birthday,"%d/%m/%Y").date()
+#birthdate = datetime.datetime.strptime(birthday,"%d/%m/%Y").date()
 
-print ("Your birth month is " + birthdate.strftime('%B'))
+#print ("Your birth month is " + birthdate.strftime('%B'))
 
+nextBirthday = datetime.datetime.strptime('12/20/2010','%m/%d/%Y').date()
+currentDate = datetime.date.today()
+
+print (nextBirthday - currentDate)
 
