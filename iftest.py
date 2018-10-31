05d3e993a21cb479c19b56e612852a4b51838743
MunYeen <munyeen.chong@pslgroup.com>
2018-10-31 11:27:18 +0800

    Date Functions

---
+++ iftest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:18:15 2018
+
+@author: munyeen.chong
+"""
+
+answer=input("Would you like express shipping?")
+
+if answer == "yes" :
+
+    print ("That will be an extra $10")
+    
+print("Thank you")
+
+    
