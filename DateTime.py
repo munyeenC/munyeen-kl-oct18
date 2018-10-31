57cd6244edf546cf1f4ed16cf52eb80feeeea4a2
MunYeen <munyeen.chong@pslgroup.com>
2018-10-31 11:10:38 +0800

    Date Functions

---
+++ DateTime.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:07:47 2018
+
+@author: munyeen.chong
+"""
+
+import datetime
+
+currentTime = datetime.datetime.now()
+
+print (currentTime)
+print (currentTime.hour)
+print (currentTime.minute)
+print (currentTime.second)
